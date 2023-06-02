13d00e4ba01da01b925b8dd5b1e56ba84bc590ea
NicoDora <76510679+NicoDora@users.noreply.github.com>
2023-06-02 09:25:29 +0900

    완

--- 3일차/자릿수_더하기.js
+++ 3일차/자릿수_더하기.js
@@ -1,12 +1,12 @@
 function solution(n)
 {
     let sum = 0;
-    while (n >= 10) {
-        tem = n % 10;
-        sum += tem;
+    let str = n.toString(); // 숫자를 문자열로 변환
+    for(let i = 0; i < str.length; i++) {
+        sum += parseInt(str[i]); // 문자열을 숫자로 변환 후 덧셈
     }
     return sum;
 }
 
-let n = 123;
+let n = 987;
 console.log(solution(n));
