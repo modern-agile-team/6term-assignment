57670de61bd9ae126b51f096393da9e15636cf34
zzzRYT <jinjinstar3@gmail.com>
2023-06-29 17:31:40 +0900

    todolist 버튼구현

--- coding-test/14일차/app/src/public/js/home/todolist.js
+++ coding-test/14일차/app/src/public/js/home/todolist.js
@@ -1,1 +1,42 @@
 "use strict";
+
+const plusBtn = document.querySelector('#button');
+plusBtn.addEventListener('click', () => {
+    const input = document.querySelector('#input');
+    const text = input.value.trim();
+
+    if(text !== "") {
+        addText(text);
+        input.value = '';
+    } else {
+        alert('입력이 필요합니다.');
+    }
+
+    return newBox;
+});
+
+function addText(text) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('divItem');
+
+    //check박스 만들기
+    const newCheckBox = document.createElement('input');
+    newCheckBox.setAttribute('type','checkbox');
+
+    //텍스트 넣기
+    const newText = document.createTextNode(text);
+
+    //수정 
+    const revise = document.createElement('input');
+    revise.classList.add('revise')
+    revise.setAttribute('type','button');
+    revise.value = "✏"
+
+    //삭제
+    // const delete = document.createElement('input')
+
+    //div에 넣기
+    newDiv.append(newCheckBox ,newText, revise);
+
+    document.querySelector('.s-box').appendChild(newDiv);      
+  }
