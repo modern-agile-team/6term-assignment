6a489e0d4370cec493d5f70071e4a17106f2ef1f
zzzRYT <jinjinstar3@gmail.com>
2023-07-06 13:41:39 +0900

    파일 정리

--- coding-test/14일차/app/src/routes/home/home.ctrl.js
+++ coding-test/14일차/app/src/routes/home/home.ctrl.js
@@ -4,31 +4,12 @@
 const Todo = require('../../models/Todo');
 
 const output = {
-    home: (req, res) => {
-        res.render('home/index');
-    },
-    login: (req, res) => {
-        res.render('home/login');
-    },
-    register: (req, res)=> {
-        res.render('home/register');
-    },
     todolist: (req, res)=> {
         res.render('home/todolist');
     }
 };
 
 const process = {
-    // login: (req, res) => {
-    //     const user = new User(req.body);
-    //     const response = user.login();
-    //     return res.json(response);
-    // },
-    // register: (req, res) => {
-    //     const user = new User(req.body);
-    //     const response = user.register();
-    //     return res.json(response);
-    // },
     todolist: async (req, res) => {
         const todo = new Todo(req.body);
         const response = await todo.todolist();
--- coding-test/14일차/app/src/routes/home/index.js
+++ coding-test/14일차/app/src/routes/home/index.js
@@ -5,14 +5,9 @@ const router = express.Router();
 
 const ctrl = require('./home.ctrl');
 
-router.get("/", ctrl.output.home);          //조회
-router.get("/login", ctrl.output.login);
-router.get("/register", ctrl.output.register);
-router.get("/todolist", ctrl.output.todolist);
+router.get("/todolist", ctrl.output.todolist); //조회
 router.get("/loadtodo", ctrl.process.loadtodo);
 
-// router.post("/login", ctrl.process.login);
-// router.post("/register", ctrl.process.register);
 router.post("/todolist", ctrl.process.todolist); //데어터 추가
 
 router.patch("/checkTodo", ctrl.process.checktodo); //수정
